82dcc02a6c7b43bcdd5fbb6f0ed8c801da48a87f
齊尼曼 <cscs851210max@gmail.com>
2022-05-30 00:16:18 +0800

    footer完成

--- js/vue.js
+++ js/vue.js
@@ -84,50 +84,50 @@ const viewmodel=Vue.createApp({
                     img_path:'./pics/gaming_card/card5.jpg',
                     img_alt_text:'電競產品周邊商品照',
                     span:'新上市',
-                    name:'人體工學耳機手柄組',
-                    text:'極致手感，您值得擁有',
-                    link:'立即擁有，數量有限'
+                    name:'死亡龐克環繞低音',
+                    text:'如入其境的沉靜式體驗',
+                    link:'立刻購買，數量有限'
                 },
                 {
                     img_path:'./pics/gaming_card/card6.jpg',
                     img_alt_text:'電競產品周邊商品照',
-                    span:'新上市',
-                    name:'無限急殺滑鼠',
-                    text:'一旦開始就無法停止遊玩',
+                    span:'預售中',
+                    name:'彩虹電幻獨角獸手柄',
+                    text:'如夢如幻的電競體驗，為女玩家量身訂造',
                     link:'立即擁有，數量有限'
                 },
                 {
                     img_path:'./pics/gaming_card/card7.jpg',
                     img_alt_text:'電競產品周邊商品照',
                     span:'超限量',
-                    name:'天籟人聲耳機',
-                    text:'極致美音，細膩分辨',
+                    name:'精造人體工學電競椅',
+                    text:'完美曲度，支持您前進的強大力量',
                     link:'立即搶購'
                 },
                 {
                     img_path:'./pics/gaming_card/card8.jpg',
                     img_alt_text:'電競產品周邊商品照',
                     span:'最熱銷',
-                    name:'黑暗主機遊戲手柄',
-                    text:'魔幻製成，餘韻無窮',
+                    name:'靈魂附魔+9滑鼠',
+                    text:'如同矮人族巫師打造，具有神奇的力量',
                     link:'立即搶購'
                 },
             ],
             onestore:[
                 {
                     img_path:'./pics/onestore/onestore1.jpg',
-                    title:'Extremely cool pc',
-                    link:'立刻購買'
+                    title:'最棒的電競體驗，不僅來自於科技，也來自人性',
+                    link:'了解詳情'
                 },
                 {
                     img_path:'./pics/onestore/onestore2.jpg',
-                    title:'Very good pc',
-                    link:'數量有限'
+                    title:'我們經營的每分每秒，都在堅持作對的事',
+                    link:'品牌堅持'
                 },
                 {
                     img_path:'./pics/onestore/onestore3.jpg',
-                    title:'Best Gaming',
-                    link:'即將售罄'
+                    title:'超過20名的電競選手一致推薦，最棒的電競品牌，魔幻電競',
+                    link:'前往購買'
                 }
             ],
             // 電競室的照片
